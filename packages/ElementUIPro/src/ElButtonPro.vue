99b70bc4a80e5e8711bc5647e364bd1d6c4cd713
dengtao@e-byte.com <1215287229@qq.com>
2024-11-07 11:10:18 +0800

    fix: fix button default slot unuseable

--- packages/ElementUIPro/src/ElButtonPro.vue
+++ packages/ElementUIPro/src/ElButtonPro.vue
@@ -1,19 +1,23 @@
 <template>
   <el-button ref="btn" v-bind="$attrs" :loading="loading" @click="_handleClick">
+    <slot>Button</slot>
   </el-button>
 </template>
 
 <script>
 import { passRef } from "./mixin/slot";
 export default {
-  name: "ElButonPro",
-  mixins: [passRef("btn")],
+  name: "ElButtonPro",
   data() {
     return {
       loading: false,
     };
   },
   emit: ["click"],
+  mounted() {
+    console.log("slots", this.$slots);
+    console.log("scopedSlots", this.$scopedSlots);
+  },
   methods: {
     _handleClick(ev) {
       this.loading = true;
