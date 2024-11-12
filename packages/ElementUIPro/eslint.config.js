b44115df45555145a2576e3a64a86ed5be4cc08b
cookingBird <d17381844023@gmail.com>
2024-11-12 22:17:34 +0800

    chore: 修复了eslint配置文件的问题，使vscode插件能够加载本地配置文件

--- packages/ElementUIPro/eslint.config.js
+++ packages/ElementUIPro/eslint.config.js
@@ -1,15 +1,33 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true,
+module.exports = [
+  {
+    root: true,
+    env: {
+      node: false,
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended'],
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-unused-vars': 'error',
+    },
+    overrides: [
+      {
+        files: ['*.vue'],
+        parser: 'vue-eslint-parser',
+        parserOptions: {
+          parser: {
+            // Script parser for `<script>`
+            'js': 'espree',
+            // Script parser for `<script lang="ts">`
+            'ts': '@typescript-eslint/parser',
+            // Script parser for vue directives (e.g. `v-if=` or `:attribute=`)
+            // and vue interpolations (e.g. `{{variable}}`).
+            // If not specified, the parser determined by `<script lang ="...">` is used.
+            '<template>': 'espree',
+          },
+          sourceType: 'module',
+        },
+      },
+    ],
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended'],
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn',
-  },
-};
+];
