c42f5468f08db38aa8f1fc0afd46c2244d38905a
cookingBird <d17381844023@gmail.com>
2024-11-13 15:53:02 +0800

    chore: 暂时注释了umd格式下TerserPlugin引起的打包报错

--- packages/ElementUIPro/webpack.config.js
+++ packages/ElementUIPro/webpack.config.js
@@ -1,60 +1,79 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	devtool: 'source-map',
-	entry: {
-		ElementUiPro: path.resolve(__dirname, './src/index.js')
-	},
-	resolve: {
-		extensions: ['.js', '.mjs', '.css', '.vue'],
-		alias: {
-			"@": path.resolve(__dirname, './src')
-		},
-		fallback: {},
-	},
-	experiments: {
-		outputModule: true,
-	},
-	plugins: [new VueLoaderPlugin(), new CleanWebpackPlugin()],
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
-			{
-				test: /\.css$/i,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							esModule: false
-						}
-					},
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: ['postcss-preset-env']
-							}
-						}
-					}
-				]
-			},
-		]
-	}
-}
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, './src/index.js'),
+  resolve: {
+    extensions: ['.js', '.mjs', '.css', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    fallback: {},
+  },
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [new VueLoaderPlugin(), new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.scss$/, // 处理所有以 .scss 结尾的文件
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'css-loader', // 解析 CSS
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+          'sass-loader', // 将 Sass 编译成 CSS
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
--- packages/ElementUIPro/webpack.config.umd.js
+++ packages/ElementUIPro/webpack.config.umd.js
@@ -1,28 +1,32 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const config = require('./webpack.config.js');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const umdCfg = {
-	mode: 'production',
-	output: {
-		path: path.resolve(__dirname, 'lib/umd'),
-		filename: '[name].js',
-		library: {
-			name: '[name]',
-			type: 'umd'
-		}
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			`...`,
-			new CssMinimizerPlugin(),
-		],
-	},
-	plugins: [new MiniCssExtractPlugin()],
-}
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'lib/umd'),
+    filename: 'index.js',
+    library: {
+      name: 'ElementUIPro',
+      type: 'umd',
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      //new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
 
-module.exports = merge(config, umdCfg)
+module.exports = merge(config, umdCfg);
